bc2ed994780d1e698d5493ffc80a474871c1cd8e
Afroj <overthinker.ltd03@gmail.com>
2026-04-27 17:07:23 +0530

    Add comments in JS

---
+++ hello.js
@@ -0,0 +1,6 @@
+console.log("Hii, I am conrad.")
+//this is in-line comment.
+//It's used for leave notes for future and others in team.
+
+/* this is multiline comment.
+  it's first thing you learn in js. */
