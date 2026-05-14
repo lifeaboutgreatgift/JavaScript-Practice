4011fe3edd1366a0999eb88993046086971a0420
Afroj <overthinker.ltd03@gmail.com>
2026-05-15 00:58:45 +0530

    learn: template literals in strings

---
+++ strings.js
@@ -0,0 +1,29 @@
+// ── STRINGS IN JAVASCRIPT ─────────────────
+// String = text data
+// Anything inside quotes is a string!
+ 
+// ── 1. CREATING STRINGS ───────────────────
+let single = 'Hello Lexy!'       // single quotes
+let double = "Hello Grett!"     //double quotes
+let backtick = `Hello Alex!`    //template literal
+
+// all three are same! but backtick is most powerful!
+
+// ── 2. TEMPLATE LITERALS ──────────────────
+// backtick strings can have variables inside!
+// same as f-strings in Python!
+
+let name = "Aditi"
+let age = 20
+let country = "India"
+
+// old way (ugly!)
+console.log("Hello " + name + " you are " + age)
+
+// new way (clean and beautiful!)
+console.log(`Hello ${name} you are ${age} years old. And you are from ${country}!`)
+//Hello Aditi you are 20 years old. And you are from India!
+
+// can do math inside too!
+console.log(`Next year you'll be ${age + 1}`) //21
+
