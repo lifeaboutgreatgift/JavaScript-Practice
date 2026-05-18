00a4929103a8b44689969482b8716c401afd342b
Afroj <overthinker.ltd03@gmail.com>
2026-05-18 11:40:53 +0530

    Numbers in javascript

---
+++ numbers.js
@@ -0,0 +1,23 @@
+// ── NUMBERS IN JAVASCRIPT ─────────────────
+// Unlike Python → JS has only ONE number type
+// integers and decimals are both just "number"!
+
+// ── 1. BASIC NUMBERS ──────────────────────
+let integer = 100
+let decimal = 9.99
+let negative = -50
+let big = 10000
+
+console.log(typeof integer)   //number
+console.log(typeof decimal)  //number
+// both are just "number" in JS!
+
+// ── 2. BASIC MATH ─────────────────────────
+console.log(10 +5)  //15 addition
+console.log(10 - 5)  // 5 subtraction
+console.log(10 / 3)   //3.333... division
+console.log(10 * 5)   //50 multiplication
+console.log(10 % 3)   //1 remainder
+console.log(2 ** 10)  // 1024 power
+
+
