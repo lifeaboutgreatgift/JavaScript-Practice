54acefc3ab51537a3e8133ab39a90fcd4eac339a
Afroj <overthinker.ltd03@gmail.com>
2026-06-01 19:27:17 +0530

    conditions: learned basic and variable initialization coditions

---
+++ conditions.js
@@ -0,0 +1,37 @@
+// if else if - is a type of control flow statement.
+// it called conditional statement or in easy word ~ decision making flow.
+
+//first you declare a variable
+
+let perTablePerson = 2;
+
+if (perTablePerson <= 3 ) {
+    console.log("Would you like table for two, sir?");
+} else if ( perTablePerson <= 3 ) {
+    console.log("Would you like group table?");
+} else {
+    console.log("Sorry! we're already reserved for today!")
+}
+
+//variable initialization - creating intentionally empty box;
+
+
+let score = 10;
+let cheerUp = "Briar! Briar! Briar!" ;
+let boo = "The team score is down!" ;
+
+let finalMsg = "";
+// this is an empty variable it's  reserved slot in memory.
+
+if (score <= 5) {
+    finalMsg = boo; // here we ressignmemt that slot, in cs term - variable mutation.
+    //  In easy words, changing or overwriting the data that was perviously stored in memory slot.
+} else {
+    finalMsg = cheerUp;
+}
+
+
+
+console.log(finalMsg);
+// using console for send data out to user!
+// without adding console in last you can't see the output in device.
