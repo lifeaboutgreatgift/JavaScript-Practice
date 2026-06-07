ba0924d44e5b8482eefba77a27f07f7388aa8721
Afroj <overthinker.ltd03@gmail.com>
2026-06-08 00:18:14 +0530

    docs: explained function topic, how it works

--- functions.js
+++ functions.js
@@ -31,3 +31,74 @@ function greetUser(username, role) { // 'username' and 'role' are PARAMETERS
 }
 
 greetUser("Briar", "Mage"); // "Briar" and "Mage" are ARGUMENTS
+
+
+// *****3. Outputs: Value vs. Action Functions***
+
+/* TYPE A: Action / Side-Effect Functions
+ These do not give a value back to the code. They perform an action in the outside world.
+ Examples: console.log(), or game engine commands like drawSprites() or playBeepSound().
+*/
+
+/* TYPE B: Value Functions (The 'return' Mechanic)
+ A function using 'return' acts like a vending machine. It processes data and spits a value back out.
+ CRUCIAL RULE: You MUST catch the returned value in a variable box, or it drops to the floor and is lost!
+*/
+
+function calculateTax(price) {
+    return price * 0.05; // Hands back a raw numerical value
+}
+
+let taxAmount = calculateTax(100); // Caught! taxAmount now holds 5
+console.log(taxAmount);
+
+
+// *****4. The Absolute Return Boundaries (Dead Code)
+
+/* Rule 1: The 'return' keyword can ONLY be used inside a function. Global returns crash the script.
+ Rule 2: 'return' is an immediate Emergency Eject Button. The function halts and exits instantly.
+ Any code written directly underneath an executed return statement is completely unreachable.
+*/
+
+function testReturn() {
+    return "Apples";
+    console.log("Oranges"); // DEAD CODE! This line will never run.
+}
+
+
+
+// *****5. Memory & Architecture: The Call Stack
+
+/* JavaScript is single-threaded (can only do one thing at a time). 
+ It uses the Call Stack (like a stack of dirty dinner plates) to track where it is.
+ When a function is called, its 'plate' drops on top. The CPU only works on the top plate.
+ When a function hits 'return', that plate is popped off the stack, exposing the line underneath.
+*/
+
+function stepTwo() {
+    console.log("Inside Step Two"); // Stack: [stepTwo, stepOne]
+}
+
+function stepOne() {
+    stepTwo(); // Pauses stepOne, drops stepTwo on top of the stack
+    console.log("Back in Step One"); // Stack: [stepOne]
+}
+stepOne();
+
+
+// 6. Function Scope (The One-Way Mirror Rule)
+
+/*Variables created INSIDE a function have Local Scope (Block Scope). 
+  Code inside the function room can look out the window and read global variables.
+  Code outside in the global hallway CANNOT look inside the room; the walls act as mirrors.
+*/
+
+let globalGold = 100; // Global Scope (Hallway)
+
+function localShop() {
+    let localItem = "Shield"; // Local Scope (Locked inside the room)
+    console.log(globalGold);   // SUCCESS! The room can look out and see 100.
+}
+
+localShop();
+// console.log(localItem); //  CRASH! ReferenceError: localItem is not defined.
